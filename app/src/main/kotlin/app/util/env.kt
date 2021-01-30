c72e704f822600cefd3000d89a106e4c0cbe7b58
Martynas Petuška <martynas.petuska@outlook.com>
2021-01-30 22:26:57 +0200

    Env var fixes

--- app/src/main/kotlin/app/util/env.kt
+++ app/src/main/kotlin/app/util/env.kt
@@ -4,7 +4,7 @@ import kotlin.properties.*
 import kotlin.reflect.*
 import kotlin.reflect.full.*
 
-class EnvDelegate<T>(private val converter: (String?) -> T) : ReadOnlyProperty<Any, T> {
+class EnvDelegate<T>(private val converter: EnvDelegate<*>.(String?) -> T) : ReadOnlyProperty<Any, T> {
   private val camelSplitRegex = "(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])".toRegex()
   override fun getValue(thisRef: Any, property: KProperty<*>): T {
     val env = System.getenv()
@@ -13,10 +13,14 @@ class EnvDelegate<T>(private val converter: (String?) -> T) : ReadOnlyProperty<A
       ?: env[property.name.split(camelSplitRegex).joinToString("_") { it.toUpperCase() }]
     return converter(value)
   }
+  
+  fun findEnv(name: String) = System.getenv()[name]
 }
 
 object Env {
-  val API_URL by EnvDelegate { it ?: "http://localhost:8080/api" }
+  val API_URL by EnvDelegate {
+    it ?: findEnv("WEBSITE_HOSTNAME")?.let { host -> "https://$host/api" } ?: "http://localhost:8080/api"
+  }
   val MONGO_STRING by EnvDelegate { it ?: "mongodb://localhost:27017" }
   
   override fun toString(): String {
