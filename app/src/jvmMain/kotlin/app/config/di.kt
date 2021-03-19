5bf6038d3c5ef42033b66649cf168c68d4244b9f
Martynas Petuška <martynas.petuska@outlook.com>
2021-03-19 13:28:37 +0200

    Search Service

--- app/src/jvmMain/kotlin/app/config/di.kt
+++ app/src/jvmMain/kotlin/app/config/di.kt
@@ -2,6 +2,8 @@ package app.config
 
 import app.service.*
 import app.util.*
+import com.azure.core.credential.*
+import com.azure.search.documents.*
 import io.ktor.application.*
 import kamp.domain.*
 import org.kodein.di.*
@@ -16,5 +18,8 @@ fun Application.diConfig() = di {
 private val services by DIModule {
   bind<CoroutineClient>() with singleton { KMongo.createClient(PrivateEnv.MONGO_STRING).coroutine }
   bind<CoroutineCollection<KotlinMPPLibrary>>() with singleton { instance<CoroutineClient>().getDatabase(PrivateEnv.MONGO_DATABASE).getCollection("libraries") }
+  
+  bind() from singleton { SearchClientBuilder().endpoint(PrivateEnv.SEARCH_SERVICE_URL).credential(AzureKeyCredential(PrivateEnv.SEARCH_SERVICE_KEY)).buildAsyncClient() }
+  
   bind<LibraryService>() with scoped(CallScope).singleton { LibraryService(context, instance()) }
 }
