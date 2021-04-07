1d629ab7d8b45a0ded159a6b64aa112027369574
Martynas Petuška <martynas.petuska@outlook.com>
2021-04-07 13:53:43 +0100

    Migration to atlas

--- app/src/jvmMain/kotlin/app/index.kt
+++ app/src/jvmMain/kotlin/app/index.kt
@@ -5,7 +5,10 @@ import app.util.*
 import io.ktor.application.*
 import io.ktor.server.cio.*
 
-fun main(args: Array<String>) = EngineMain.main(args)
+fun main(args: Array<String>) {
+  System.setProperty("jdk.tls.client.protocols", "TLSv1.2")
+  EngineMain.main(args)
+}
 
 fun Application.module() {
   features()
